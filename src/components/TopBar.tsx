2932da6139324ec371d9ce3354a46f6bdea012c7
TanLui <effishing@gmail.com>
2023-01-26 17:58:22 -0500

    add click logo link to home

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,12 +3,14 @@ import * as React from 'react'
 const TopBar = () => {
   return (
     <div className='bg-white pt-2 pb-1' style={{ maxWidth: '1800px' }}>
-      <h1
-        className=' text-5xl font-bold mt-0 mb-6'
-        style={{ color: '#D38AA3' }}
-      >
-        The Milk Store
-      </h1>
+      <a href={`/`}>
+        <h1
+          className=' text-5xl font-bold mt-0 mb-6'
+          style={{ color: '#D38AA3' }}
+        >
+          The Milk Store
+        </h1>
+      </a>
     </div>
   )
 }
